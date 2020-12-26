265ab718688e6591c22bb096de994fa990398f35
inthahouse <alan.nguy@outlook.com>
2020-12-26 03:11:39 -0500

    change to space

--- hooks/use-cart.ts
+++ hooks/use-cart.ts
@@ -1,81 +1,80 @@
 import { useState, createContext, useContext } from 'react'
 import { goToCheckout } from 'utils/payment'
-import { IProduct } from 'utils/types/IProduct'
 import { LineItem } from 'utils/types/LineItem'
 import products from '../utils/products.json'
 
 interface CartDetails {
-	products: any
+  products: any
 }
 
 export const CartContext = createContext({})
 
 export const useCart = () => {
-	const [cartDetails, setCartDetails] = useState<CartDetails>({ products: {} })
+  const [cartDetails, setCartDetails] = useState<CartDetails>({ products: {} })
 
-	const cartItems = Object.keys(cartDetails.products).map((priceId) => {
-		const cartItem = products.find((product) => product.priceId === priceId)
+  const cartItems = Object.keys(cartDetails.products).map((priceId) => {
+    const cartItem = products.find((product) => product.priceId === priceId)
 
-		if (cartItem) {
-			return {
-				...cartDetails.products[priceId],
-				priceId: cartItem.priceId,
-				pricePerUnit: cartItem.price,
-			}
-		} else {
-			return {}
-		}
-	})
+    if (cartItem) {
+      return {
+        ...cartDetails.products[priceId],
+        priceId: cartItem.priceId,
+        pricePerUnit: cartItem.price,
+      }
+    } else {
+      return {}
+    }
+  })
 
-	const addToCart = (priceId: string) => {
-		setCartDetails((previous) => {
-			let cart: any = { ...previous }
+  const addToCart = (priceId: string) => {
+    setCartDetails((previous) => {
+      let cart: any = { ...previous }
 
-			if (cart.products[priceId]) {
-				cart.products[priceId].quantity = cart.products[priceId].quantity + 1
-			} else {
-				cart.products[priceId] = { quantity: 1 }
-			}
+      if (cart.products[priceId]) {
+        cart.products[priceId].quantity = cart.products[priceId].quantity + 1
+      } else {
+        cart.products[priceId] = { quantity: 1 }
+      }
 
-			return cart
-		})
-	}
+      return cart
+    })
+  }
 
-	const updateQuantity = (priceId: string, quantity: number) => {
-		setCartDetails((previous) => {
-			let cart: any = { ...previous }
+  const updateQuantity = (priceId: string, quantity: number) => {
+    setCartDetails((previous) => {
+      let cart: any = { ...previous }
 
-			if (cart.products[priceId]) {
-				cart.products[priceId].quantity = quantity
-			}
+      if (cart.products[priceId]) {
+        cart.products[priceId].quantity = quantity
+      }
 
-			return cart
-		})
-	}
+      return cart
+    })
+  }
 
-	const startCheckout = () => {
-		const lineItems: LineItem[] = cartItems.map((cartItem) => {
-			return {
-				price: cartItem.priceId,
-				quantity: cartItem.quantity,
-			}
-		})
+  const startCheckout = () => {
+    const lineItems: LineItem[] = cartItems.map((cartItem) => {
+      return {
+        price: cartItem.priceId,
+        quantity: cartItem.quantity,
+      }
+    })
 
-		goToCheckout(lineItems)
-	}
+    goToCheckout(lineItems)
+  }
 
-	return {
-		addToCart,
-		cartDetails,
-		setCartDetails,
-		startCheckout,
-		cartItems,
-		updateQuantity,
-	}
+  return {
+    addToCart,
+    cartDetails,
+    setCartDetails,
+    startCheckout,
+    cartItems,
+    updateQuantity,
+  }
 }
 
 export const useCartContext: any = () => {
-	const cart = useContext(CartContext)
+  const cart = useContext(CartContext)
 
-	return cart
+  return cart
 }
